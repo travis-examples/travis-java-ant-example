d450e34ee819d3a8a0a95e6c32b626c7766b3cbd
Dana Engel <engel.dana.mail@gmail.com>
2013-10-07 18:05:26 +0200

    customize hello-world output to trigger travis

--- src/cc/engeld/HelloWorld.java
+++ src/cc/engeld/HelloWorld.java
@@ -2,6 +2,6 @@ package cc.engeld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, I'm using Travis-CI!");
     }
 }
